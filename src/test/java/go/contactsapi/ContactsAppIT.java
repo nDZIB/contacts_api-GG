0d2bb02d875f24bbf0be984fe0f647f980c5d1c9
ndzib <brunoserkwi@gmail.com>
2019-10-27 15:15:48 -0100

    wrote integration test closes #7

---
+++ src/test/java/go/contactsapi/ContactsAppIT.java
@@ -0,0 +1,34 @@
+package go.contactsapi;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import go.contactsapi.model.Contact;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ContactsAppIT {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Test
+	public void getAllContactsEndPointReturnsListOfContacts() {
+		List<Contact> contacts = restTemplate
+				.getForObject("/contacts", ArrayList.class);
+		
+		assertThat(contacts).hasSize(2);
+	}
+
+}
